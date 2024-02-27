42be03cc02a1cf337415c9cddabd144ebd354323
Facundo Noya <facunoya@hotmail.com>
2024-02-27 18:16:20 -0300

    run server page

---
+++ Bootstrap/portfolio-web/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path')
+const app = express()
+
+app.use(express.json())
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+})
+app.listen(3024, () => { console.log('aplicacion montada en puerto 3024') })
